c535c46c4d21ef89d79643f6b0c5f8346c11b3df
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-07-03 16:54:55 -0300

    Aula com Python

---
+++ aula03.py
@@ -0,0 +1,11 @@
+minha_var = '"Não gostar de paçoca é crime inafiançável", disse meu avô'
+print(minha_var)
+
+
+x = 5
+y = 'John'
+print(type(x))
+print(type(y))
+
+if type(x) == int:
+    print(x+2)
